188117c6d68015a0d1237a0be7be521e1ad0717d
Adhya Prasad <adhyaprasadka@gmail.com>
2024-09-05 00:37:15 +0530

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+    <div className="Navbar">
+      <div className="logo">Skilledity</div>
+      <div className="buttons">
+        <button className="login">Login</button>
+      </div>
+    </div>
+      <div/>
+    </header>
+  );
+  
+}
+
+export default Header;
