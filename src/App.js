fd2874bb8ebcb8ea352334e0005f331c9124a1ca
druva-muga <druvakumar.1422@gmail.com>
2024-09-05 23:36:48 +0530

    school login page 1st commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import LoginPortal from "./components/LoginPortal";
+import SchoolLoginPage from "./components/SchoolLoginPage";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <LoginPortal />
+      <SchoolLoginPage />
     </div>
   );
 }
--- src/components/SchoolLoginPage.js
+++ src/components/SchoolLoginPage.js
@@ -1,77 +1,36 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-const onFinish = (values) => {
-  console.log('Success:', values);
+import './SchoolLoginPage.css'; // Add CSS for styling the page
+
+const SchoolLoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h1>Welcome!</h1>
+        <h3>Sign in to</h3>
+        <p> the School Login portal</p>
+        <div>
+        <form className='container'> 
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="Enter your username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your Password" />
+          </div>
+          <div className="form-group2">
+            <input type="checkbox" id="remember-me" />
+            <label id="rememberme" htmlFor="remember-me">Remember Me</label>
+          </div>
+          <button type="submit">Log in</button>
+        </form>
+        </div>
+      </div>
+      <div className="login-image">
+        <img src="./SVGs/undraw_enter_uhqk.svg" alt="Login illustration" />
+      </div>
+    </div>
+  );
 };
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-const App = () => (
-  <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
-export default App;
+export default SchoolLoginPage;
