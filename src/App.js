52a26357c95be23c9cfd58b45afae3fa81a31ee6
Pranesh-2854 <118817661+Pranesh-2854@users.noreply.github.com>
2024-09-11 22:25:40 +0530

    Linking the ManualReg Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./components/Dashboard";
 import ForgotPasswordPage from "./components/ForgortPasswordPage";
 import { DataProvider } from './context/dataContext';
 import UploadCSV from './components/UploadCSV';
-
+import Register from './components/ManualReg';
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="forgot-password" element={<ForgotPasswordPage />} />
             <Route path="upload-csv" element={<UploadCSV />} />
+           <Route path="manual-register-student" element={<Register/>}/>
 
           </Routes>
         </div>
