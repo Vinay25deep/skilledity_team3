ca0941533a66f7bad64ed923e4dc827301db1d16
uankit22 <uankit281@gmail.com>
2024-09-14 14:08:53 +0530

    Added next pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import LoginPortal from "./components/LoginPortal";
 import SchoolLoginPage from "./components/SchoolLoginPage";
 import Dashboard from "./components/Dashboard";
 import ForgotPasswordPage from "./components/ForgortPasswordPage";
-import { DataProvider } from './context/dataContext';
-import UploadCSV from './components/UploadCSV';
-import Register from './components/ManualReg';
-import Welcome from './components/WelcomePg';
-import ManualStudentDetails from './components/ManualRegStudentDetails';
+import { DataProvider } from "./context/dataContext";
+import UploadCSV from "./components/UploadCSV";
+import Register from "./components/ManualReg";
+import Welcome from "./components/WelcomePg";
+import ManualStudentDetails from "./components/ManualRegStudentDetails";
 function App() {
   return (
     <DataProvider>
@@ -17,15 +17,14 @@ function App() {
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path='/' element={<LoginPortal />} />
+            <Route path="/" element={<LoginPortal />} />
             <Route path="school-login" element={<SchoolLoginPage />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="forgot-password" element={<ForgotPasswordPage />} />
             <Route path="upload-csv" element={<UploadCSV />} />
-            <Route path="welcome" element={<Welcome/>} />
-            <Route path="manual-register-student" element={<Register/>}/>
-            <Route path="manual-register-students" element={<ManualStudentDetails/>}/>
-
+            <Route path="welcome" element={<Welcome />} />
+            <Route path="manual-register-student" element={<Register />} />
+            <Route path="manual-register-students" element={<ManualStudentDetails />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/ManualRegStudentDetails.js
+++ src/components/ManualRegStudentDetails.js
@@ -1,33 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./ManualRegStudentDetails.css";
+import SidebarForm from "./SidebarForm";
 
 const ManualStudentDetails = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const showForm = () => setIsFormVisible(true);
+  const hideForm = () => setIsFormVisible(false);
+
   return (
-    <div className="container-students">
-      <div className="containerContent">
-        <h1 className="studentDetails">
-          Student <span className="detailsSpan">Details!</span>
-        </h1>
-        <div className="containerSubContent">
-          <div className="containerDesc">
-            <h1 className="schoolNameTitle">School Name</h1>
-            <div className="containerSchoolClassSection">
-              <h1 className="schoolName">Apeejay School, NOIDA</h1>
-              <h1 className="classSection">Class & Section</h1>
+    <div className="containerManualRegStudentDetails">
+      {isFormVisible && <SidebarForm onClose={hideForm} />}
+      <div className="container-students">
+        <div className="containerContent">
+          <h1 className="studentDetails">
+            Student <span className="detailsSpan">Details!</span>
+          </h1>
+          <div className="containerSubContent">
+            <div className="containerDesc">
+              <h1 className="schoolNameTitle">School Name</h1>
+              <div className="containerSchoolClassSection">
+                <h1 className="schoolName">Apeejay School, NOIDA</h1>
+                <h1 className="classSection">Class & Section</h1>
+              </div>
             </div>
-          </div>
-          <div className="containerDisplayStudents">
-            <h1 className="students">Students</h1>
-            <div className="widgetStudents">
-              <div className="smallWidgetName">S</div>
-              <h1 className="studentName">S Chandramouli</h1>
+            <div className="containerDisplayStudents">
+              <h1 className="students">Students</h1>
+              <div className="widgetStudents">
+                <div className="smallWidgetName">S</div>
+                <h1 className="studentName">S Chandramouli</h1>
+              </div>
             </div>
-          </div>
-          <Link to="/">
-            <div className="addStudentsBtn">
+            <div className="addStudentsBtn" onClick={showForm}>
               <span className="plusIcon">+ </span> Add Student
             </div>
-          </Link>
+          </div>
         </div>
       </div>
     </div>
